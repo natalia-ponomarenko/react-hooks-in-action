fe9feb2912d224e6c3c4e0bbf1a532f2d9fc9c9c
natalia-ponomarenko <virus.zabutiya@gmail.com>
2022-12-05 11:36:51 +0200

    add Week-picker component

---
+++ src/components/Bookings/BookingsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WeekPicker from './WeekPicker';
+
+export default function BookingsPage() {
+  return (
+    <main className="bookings-page">
+      <WeekPicker date={new Date()} />
+    </main>
+  );
+}
